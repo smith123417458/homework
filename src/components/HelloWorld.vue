a5bfcb77731b2cab189cd631df032f484cd8d8ad
盛建雄 <a0952987429@yahoo.com.tw>
2021-05-23 21:30:00 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,95 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <v-app id="inspire">
+      <v-card class="mx-auto" max-width="400">
+        <v-list two-line>
+          <v-subheader class="title">
+            <v-btn icon>
+              <v-icon color="green">mdi-run</v-icon>
+            </v-btn>
+            進行中</v-subheader
+          >
+          <template v-for="(item, index) in doing">
+            <v-list-item :key="item.name">
+              <v-list-item-avatar>
+                <v-img :src="item.logo"></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <span>
+                  <span class="wordGreen">{{ item.status.type }} </span>
+                  <span style="color: white"> -------- </span> 預計出貨日:{{ item.date }}
+                </span>
+                <p style="margin-top: 2%; line-height: 1.5">{{ item.name }}</p>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon color="grey lighten-1">mdi-chevron-right</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+
+            <v-divider v-if="index < doing.length - 1" :key="index"></v-divider>
+          </template>
+
+          <v-subheader class="title">
+            <v-btn icon> <v-icon color="green">mdi-thumb-up-outline</v-icon> </v-btn
+            >已完成</v-subheader
+          >
+
+          <template v-for="(item, index) in finished">
+            <v-list-item :key="item.name">
+              <v-list-item-avatar>
+                <v-img src="../assets/logo.png"></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <span class="wordGreen">{{ item.status.type }} </span>
+
+                <p style="margin-top: 2%; line-height: 1.5">{{ item.name }}</p>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon color="grey lighten-1">mdi-chevron-right</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+
+            <v-divider v-if="index < finished.length - 1" :key="index.name"></v-divider>
+          </template>
+        </v-list>
+      </v-card>
+    </v-app>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data: () => ({
+    doing: [],
+    finished: [],
+  }),
+  created() {
+    this.rawDate = this.$store.state.getDate;
+    if (this.rawDate.length !== 0) {
+      this.doing = this.rawDate.filter((e) => e.status.code == 1 || e.status.code == 2);
+      this.doing.sort((a, b) => a.date.localeCompare(b.date));
+      this.finished = this.rawDate.filter(
+        (e) => e.status.code == 3 || e.status.code == 4
+      );
+    }
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style scoped>
+.wordGreen {
+  color: green;
+  font-weight: bold !important;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.title {
+  background-color: rgb(231, 230, 230);
+  font-weight: bold !important;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,70 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 Vue.use(Vuex)
-
 export default new Vuex.Store({
   state: {
+    getDate: [
+      {
+        logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
+        name: "Livi優活 抽取式衛生紙(100抽x10包x10串/箱)",
+        status: {
+          code: 3,
+          type: "已取消",
+        },
+        date: "107/6/12",
+      },
+      {
+        logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
+        name: "BALMUDA The Toaster 百慕達烤麵包機-黑色",
+        status: {
+          code: 2,
+          type: "已成立",
+        },
+        date: "108/7/21",
+      },
+      {
+        logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
+        name: "贈-短慧萬用鍋HD2133+三合一濾網「LG樂金」韓國原裝...",
+
+        status: {
+          code: 4,
+          type: "已送達",
+        },
+        date: "106/3/2",
+      },
+      {
+        logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
+        name: "Apple AirPds 2",
+        status: {
+          code: 2,
+          type: "處理中",
+        },
+        date: "108/6/7",
+      },
+
+      {
+        name: "Travis Howard 贈三合一濾網「LG樂金」韓國原裝...",
+        logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
+        status: {
+          code: 2,
+          type: "己送達",
+        },
+        date: "108/6/2",
+      },
+      {
+        name: "小米手錶 贈-短慧萬用鍋HD2133+三合一濾網",
+        logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
+        status: {
+          code: 1,
+          type: "己取消",
+        },
+        date: "108/6/2",
+      },
+    ],
+
   },
   mutations: {
   },
   actions: {
   },
-  modules: {
-  }
 })
